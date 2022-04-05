1de96c59c97cfbab9a32f2f1d1e8a4d6904c3978
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-04-06 01:18:41 +0400

    idk

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Referencing nodes
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors')
+
+
+function reveal(){
+    console.log('rock')
+}
+
+rock.addEventListener('click', reveal);
+
+
+
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     let theChoice = choices[Math.floor(Math.random() * choices.length)]
@@ -6,8 +21,7 @@ function computerPlay() {
 
 
 
-function gameLoop(playerSelection, computerSelection) {
-    let announce;
+function playRound(playerSelection, computerSelection) {
     let noPoints = false;
     let win;
     // let playerPoints;
@@ -35,31 +49,31 @@ function gameLoop(playerSelection, computerSelection) {
 
 }
 
-function game() {
-    let score = 0;
-    let cpuScore = 0;
-    let announce;
-    let playOrNay = confirm("Play or Cancel?");
-    let playerSelection;
-    if (playOrNay || playerSelection) {
-        for (let i = 0; i < 5; i++) {
-            playerSelection = prompt("Please Enter Your Choice", "Rock, Paper Or Scissors").toLowerCase(); // takes user input
-            const computerSelection = computerPlay(); // Computer Selection
-            gameLoop(playerSelection, computerSelection) ? score++ : cpuScore++; // increments player and computer scores
-            announce = alert(`Player Chose: ${playerSelection}\nComputer Chose: ${computerSelection}`);
-            if (playerSelection == computerSelection ) {
-                score = score; 
-                cpuScore = cpuScore;
-                announce = alert(`It's a Tie!`);
-            }
-        };
-        score > cpuScore ? announce = `Player Score: ${score}\nComputer Score: ${cpuScore}\nYou Won!` :
-            announce = `Player Score: ${score}\nComputer Score: ${cpuScore}\nComputer Won!`
-    } else {
-        announce = `Player Score: ${score}\nComputer Score: ${cpuScore}`;
-        announce = "You Quit";
-    }
-    alert(announce);
-}
+// function game() {
+//     let score = 0;
+//     let cpuScore = 0;
+//     let announce;
+//     let playOrNay = confirm("Play or Cancel?");
+//     let playerSelection;
+//     if (playOrNay || playerSelection) {
+//         for (let i = 0; i < 5; i++) {
+//             playerSelection = prompt("Please Enter Your Choice", "Rock, Paper Or Scissors").toLowerCase(); // takes user input
+//             const computerSelection = computerPlay(); // Computer Selection
+//             playRound(playerSelection, computerSelection) ? score++ : cpuScore++; // increments player and computer scores
+//             announce = alert(`Player Chose: ${playerSelection}\nComputer Chose: ${computerSelection}`);
+//             if (playerSelection == computerSelection ) {
+//                 score = score; 
+//                 cpuScore = cpuScore;
+//                 announce = alert(`It's a Tie!`);
+//             }
+//         };
+//         score > cpuScore ? announce = `Player Score: ${score}\nComputer Score: ${cpuScore}\nYou Won!` :
+//             announce = `Player Score: ${score}\nComputer Score: ${cpuScore}\nComputer Won!`
+//     } else {
+//         announce = `Player Score: ${score}\nComputer Score: ${cpuScore}`;
+//         announce = "You Quit";
+//     }
+//     alert(announce);
+// }
 
-game();
+// game();
