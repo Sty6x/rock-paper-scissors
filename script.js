e7759b2ab885f3124a19f64a41021e0d4fa6394a
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-04-06 23:53:45 +0400

    Storing button choices in playerSelection variable using switch statements

--- script.js
+++ script.js
@@ -16,51 +16,27 @@ function computerPlay() {
 }
 
 
-// function playRound(playerSelection, computerSelection) {
-//     let noPoints = false;
-//     let win;
-//     if (playerSelection == computerSelection) {
-//         noPoints = true;
-//     } else if (playerSelection == 'rock' & computerSelection == 'scissors') {
-//         win = true;
-//     } else if (playerSelection == 'rock' & computerSelection == 'paper') {
-//         win = false;
-//     } else if (playerSelection == 'paper' & computerSelection == 'rock') {
-//         win = true;
-//     } else if (playerSelection == 'paper' & computerSelection == 'scissors') {
-//         win = false;
-//     } else if (playerSelection == 'scissors' & computerSelection == 'rock') {
-//         win = false;
-//     } else if (playerSelection == 'scissors' & computerSelection == 'paper') {
-//         win = true;
-//     }
-//     return playerSelection
-// }
-
-
-buttons.forEach((playerSelection) => {
-    playerSelection.addEventListener('click', (e) => {
-        let win;
-        let noPoints;
-        if (e.target.id == computerPlay()) {
-            noPoints = true;
-        } else if (e.target.id == 'rock' & computerPlay() == 'scissors') {
-            win = true;
-        } else if (e.target.id == 'rock' & computerPlay() == 'paper') {
-            win = false;
-        } else if (e.target.id == 'paper' & computerPlay() == 'rock') {
-            win = true;
-        } else if (e.target.id == 'paper' & computerPlay() == 'scissors') {
-            win = false;
-        } else if (e.target.id == 'scissors' & computerPlay() == 'rock') {
-            win = false;
-        } else if (e.target.id == 'scissors' & computerPlay() == 'paper') {
-            win = true;
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let playerSelection = '';
+        switch (e.target.id) {
+            case 'rock':
+                playerSelection = 'rock';
+                break;
+            case 'paper':
+                playerSelection = 'paper'
+                break;
+            case 'scissors':
+                playerSelection = 'scissors'
+                break;
         }
-        console.log(e.target.id, computerPlay(), win, noPoints);
+
+        console.log(playerSelection);
+
     })
 })
 
+// console.log(computerPlay());
 
 // a function that changes the playerSelection to either rock paper or scissors
 
@@ -74,4 +50,3 @@ buttons.forEach((playerSelection) => {
 // function buttonClick(e){
 //     console.log(typeof e.target.id);
 // }
-
