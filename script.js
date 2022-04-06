2465d3a3cf39b605aead04f78b37bf9a44d8f092
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-04-07 02:09:02 +0400

    finished logic

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
 // Referencing nodes
-// const rock = document.querySelector('#rock');
-// const paper = document.querySelector('#paper');
-// const scissors = document.querySelector('#scissors')
 const buttons = document.querySelectorAll('button');
+const buttonContainer = document.querySelector('.buttons');
+const showPlayerScore = document.createElement('div');
+const showEnemyScore = document.createElement('div');
+const tieScore = document.createElement('div');
+const body = document.querySelector('body')
+body.appendChild(showPlayerScore);
+body.appendChild(showEnemyScore)
+body.appendChild(tieScore);
 
 
+// declaring variables
+let playerScore = 0;
+let computerScore = 0;
 
 // Add the fuction gameplay on for each loop on addeventlistener
 
 
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
-    // let theChoice = choices[Math.floor(Math.random() * choices.length)]
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 
 function playRound(playerSelection, computerSelection) {
-    
-
-    
-
-    // if (playerSelection == computerSelection){
-    //     console.log(`its a tie: \nplayer: ${playerSelection} \ncomputer: ${computerSelection}`)
-    // }
-
-    console.log(playerSelection, computerSelection);
+    // player
+    if (playerSelection == 'rock' && computerSelection == 'scissors' ||
+        playerSelection == 'scissors' && computerSelection == 'paper' ||
+        playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore += 1;
+        body.removeChild(tieScore);
+        showPlayerScore.textContent = `player Score: ${playerScore}`;
+        if (playerScore == 5){
+            body.innerHTML = '<span> Player Wins: 5 </span>'
+        }
+        
+    } else if (playerSelection == computerSelection) {
+        body.appendChild(tieScore);
+        tieScore.textContent = `It's a tie!`
+    }
+    // computer
+    else {
+        computerScore += 1
+        showEnemyScore.textContent = `cpu Score: ${computerScore}`;
+        if(computerScore == 5){
+            body.innerHTML = '<span> Enemy Wins: 5 </span>'
+        }
+    }
 }
-
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         playRound(e.target.id, computerPlay())
+        
     })
 });
-
-
